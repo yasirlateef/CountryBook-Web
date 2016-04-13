fde9724b4d9298f61242f56a4aff61fad304a706
Yasir Lateef <yasirlateef112@gmail.com>
2016-04-13 21:18:34 +0500

    Updated code for flag images

--- public/src/controllers/countriesCtrl.js
+++ public/src/controllers/countriesCtrl.js
@@ -1,22 +1,28 @@
 var countriesCtrl = angular.module('countriesCtrl', ['ngRoute', 'countriesService']);
 
+countriesCtrl.run(function($rootScope){
+	$rootScope.host = 'http://localhost:8888';
+});
+
 countriesCtrl.controller('countriesController', function($scope, countries){
 	countries.list(function(countries){
 		$scope.countries = countries;
 	});
 });
 
-countriesCtrl.controller('countriesDetailController', function($scope, $routeParams, countries){
+countriesCtrl.controller('countriesDetailController', function($rootScope, $scope, $routeParams, countries){
 	countries.find($routeParams.countryName, function(country){
 		$scope.country = country;
 		$scope.isArray = Array.isArray;
+		$scope.host = $rootScope.host;
 	});
 });
 
-countriesCtrl.controller('countriesAttribController', function($scope, $routeParams, countries){
+countriesCtrl.controller('countriesAttribController', function($rootScope, $scope, $routeParams, countries){
 	countries.findAttribute($routeParams.countryName, $routeParams.attribName, function(res){
 		$scope.countryName = $routeParams.countryName;
 		$scope.attribName = $routeParams.attribName;
 		$scope.response = res;
+		$scope.host = $rootScope.host;
 	});
 });
